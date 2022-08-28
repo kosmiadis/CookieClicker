b7f7e9f9a0a7a8c8342b135ccdc4dfd676c62ea4
VagelisKosmiadis <vageliskosmiadis@gmail.com>
2022-08-28 20:02:55 +0300

    Changed animation scale

--- JS/animateCookie.js
+++ JS/animateCookie.js
@@ -1,7 +1,7 @@
 const cookieImg = document.querySelector('#cookie')
 
 function animateCookie (cookie) {
-    cookie.style.transform = 'scale(1.1)'
+    cookie.style.transform = 'scale(1.05)'
     setTimeout(function getToNormalSize() {
         cookie.style.transform = 'scale(1)'
     },50)
